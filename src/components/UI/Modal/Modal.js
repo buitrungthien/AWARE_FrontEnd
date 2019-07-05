5016ed572e55d000c948280997627bf6d5d6d652
buitrungthien <thienbt95@gmail.com>
2019-07-05 15:17:31 +0700

    added a cross-icon to do the closing function in addition to the backdrop.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,12 +2,16 @@ import React from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 import Auxxx from '../../../hoc/Auxxx/Auxxx';
+import CloseIcon from '../../../assets/images/icons/cross.svg';
 
 const modal = (props) => {
     return (
         <Auxxx>
             <Backdrop show={props.show} clicked={props.closeModal}/>
             <div className={classes['modal']}>
+                <i onClick={props.closeModal} style={{float: 'right', padding: '10px'}}>
+                    <img src={CloseIcon} alt=""></img>
+                </i>
                 {props.children}
             </div>
         </Auxxx>
